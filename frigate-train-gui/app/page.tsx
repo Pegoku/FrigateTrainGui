41587da39a96d26cc79afba8e8f9846126039124
Pegoku <pegoku08@gmail.com>
2026-01-07 10:57:15 +0100

    now I can get the "most frequent" face in the timestamp

--- frigate-train-gui/app/page.tsx
+++ frigate-train-gui/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Face from "@/components/Face";
 import { main } from "bun";
 import { getFaceData } from "@/utils/main";
-import type  { FacesMap, FaceData} from "@/types";
+import type { FacesMap, FaceData } from "@/types";
 
 export default async function Home() {
   const faceName = "train";
@@ -13,35 +13,55 @@ export default async function Home() {
   const groupedFaces: Record<string, string[]> = {};
   const timestamps: string[] = [];
   for (const face of faces) {
-      const timestamp: string = face.split("-")[0] as string;
-      if (!groupedFaces[timestamp]) {
-          groupedFaces[timestamp] = [];
-          timestamps.push(timestamp);
-      }
-      groupedFaces[timestamp].push(face);
+    const timestamp: string = face.split("-")[0] as string;
+    if (!groupedFaces[timestamp]) {
+      groupedFaces[timestamp] = [];
+      timestamps.push(timestamp);
+    }
+    groupedFaces[timestamp].push(face);
   }
   for (const ts of timestamps) {
-      const names = groupedFaces[ts].map((face) => face.split("-")[3]);
-      console.log(`Timestamp: ${ts}, Names: ${names.join(", ")}`);
-      
+    // const names = groupedFaces[ts].map((face) => face.split("-")[3]);
+    const names: Record<string, number> = {};
+    for (const face of groupedFaces[ts].map((face) => face.split("-")[3])) {
+      if (!names[face]) {
+        names[face] = 0;
+      }
+      names[face] += 1;
+    }
+
+    const maxCount = Math.max(...Object.values(names), 0);
+    const maxName = Object.entries(names)
+      .filter(([, count]) => count == maxCount)
+      .map(([name]) => name)[0];
+
+    console.log(
+      `Timestamp: ${ts}, Names: ${JSON.stringify(names)}, Max name: ${maxName}, Max count: ${maxCount}`
+    );
   }
 
   return (
     <main>
       {Object.entries(groupedFaces).map(([timestamp, faceList]) => (
-        <div key={timestamp} className="m-1 bg-zinc-900 rounded-lg inline-block">
-        {faceList.map((face) => {
-          const faceData: FaceData =
-            face.split("-") as FaceData;
-          return (
-            <Face
-              key={face}
-              img={`/api/face-image?faceName=${faceName}&face=${face}`}
-              name={faceData[3]}
-              percent={((parseFloat(faceData[4].split(".webp")[0]) * 100).toFixed(0) as unknown) as number}
-            />
-          );
-        })}
+        <div
+          key={timestamp}
+          className="m-1 bg-zinc-900 rounded-lg inline-block"
+        >
+          {faceList.map((face) => {
+            const faceData: FaceData = face.split("-") as FaceData;
+            return (
+              <Face
+                key={face}
+                img={`/api/face-image?faceName=${faceName}&face=${face}`}
+                name={faceData[3]}
+                percent={
+                  (parseFloat(faceData[4].split(".webp")[0]) * 100).toFixed(
+                    0
+                  ) as unknown as number
+                }
+              />
+            );
+          })}
         </div>
       ))}
     </main>
