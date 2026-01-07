a576057d40e35a1e58f3c9fb579dbda2412566ac
Pegoku <pegoku08@gmail.com>
2026-01-07 14:02:04 +0100

    tests with Radix

--- frigate-train-gui/components/Header.tsx
+++ frigate-train-gui/components/Header.tsx
@@ -8,8 +8,27 @@ interface HeaderProps {
 export default function Header() {
     return (
         <header>
-            <p>Hello world</p>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                    <button className="p-2 m-2 bg-zinc-800 rounded-lg">
+                        Selected Face
+                        </button>
+                </DropdownMenu.Trigger>
 
+                <DropdownMenu.Portal>
+                    <DropdownMenu.Content className="bg-zinc-800 p-2 rounded-lg">
+                        <DropdownMenu.Item>
+                            Hello World
+                        </DropdownMenu.Item>
+                        <DropdownMenu.Separator className="border-zinc-700"/>
+                        <DropdownMenu.Label className="text-zinc-400 mb-2">label</DropdownMenu.Label>
+                        
+                        <DropdownMenu.Item>
+                            Hello World
+                        </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+            </DropdownMenu.Root>
         </header>
 
     )
