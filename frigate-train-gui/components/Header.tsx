bd771476aeed510c03368404ffc7b5f7c2fc1d44
Pegoku <pegoku08@gmail.com>
2026-01-16 21:18:23 +0100

    blur names

--- frigate-train-gui/components/Header.tsx
+++ frigate-train-gui/components/Header.tsx
@@ -26,7 +26,7 @@ export default function Header({ faces }: HeaderProps) {
                             Object.entries(faces).map(([name, count]) => (
                                 name !== "train" && (
                                     <DropdownMenu.Item key={name} className="h-9 flex items-center justify-between min-w-50">
-                                        <span className="capitalize">{name}</span>
+                                        <span className="capitalize blur">{name}</span>
                                         <span className="text-xs text-zinc-400"> ({count})</span>
                                     </DropdownMenu.Item>
                                 )
