ccc70b56000006fdee274632c6b4a3b51d14ab76
Pegoku <pegoku08@gmail.com>
2026-01-18 01:26:04 +0100

    add selectedFace useState

--- frigate-train-gui/components/Header.tsx
+++ frigate-train-gui/components/Header.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { DropdownMenu, Tooltip } from "radix-ui";
+import { useState } from "react";
 
 interface HeaderProps {
     faces: Record<string, number>;
-    selectedFace: string;
+    // selectedFace: string;
 }
 
-export default function Header({ faces, selectedFace="train" }: HeaderProps) {
+export default function Header({ faces}: HeaderProps) {
+
+    const [selectedFace, setSelectedFace] = useState("train");
     return (
         <header>
             <DropdownMenu.Root>
@@ -18,7 +21,7 @@ export default function Header({ faces, selectedFace="train" }: HeaderProps) {
 
                 <DropdownMenu.Portal>
                     <DropdownMenu.Content className="bg-zinc-800 p-2 rounded-lg mt-1 border-2 border-zinc-700" align="start">
-                        <DropdownMenu.Item onSelect={() => selectedFace = "train"} className="h-9 flex items-center justify-between min-w-50">
+                        <DropdownMenu.Item onSelect={() => setSelectedFace("train")} className="h-9 flex items-center justify-between min-w-50">
                             Train: {faces["train"] || 0}
                         </DropdownMenu.Item>
                         <DropdownMenu.Separator className="my-2 h-px w-full bg-zinc-700" />
@@ -26,7 +29,7 @@ export default function Header({ faces, selectedFace="train" }: HeaderProps) {
                         {
                             Object.entries(faces).map(([name, count]) => (
                                 name !== "train" && (
-                                    <DropdownMenu.Item onSelect={() => selectedFace = name} key={name} className="h-9 flex items-center justify-between min-w-50">
+                                    <DropdownMenu.Item onSelect={() => setSelectedFace(name)} key={name} className="h-9 flex items-center justify-between min-w-50">
                                         <span className="capitalize blur">{name}</span>
                                         <span className="text-xs text-zinc-400"> ({count})</span>
                                     </DropdownMenu.Item>
